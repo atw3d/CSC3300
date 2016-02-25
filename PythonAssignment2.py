4bc0321f7e00c7079aba99fa180deffa5b13649c
atw3d <townley.williams@gmail.com>
2016-02-25 10:10:40 -0500

    Update PythonAssignment2.py

--- PythonAssignment2.py
+++ PythonAssignment2.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python
 
-import sys
+def main():
+    flNames = open("name.txt", "r")
+    lstMasterNames = []
+    
+    for strLine in flNames:
+		strLine = strLine.rstrip()
+		strLastFirst = strLine.split(' ')[1] + ',' + strLine.split(' ')[0]
+		lstMasterNames.append(strLastFirst)
+		
+    lstMasterNames.sort()
+    
+    for strName in lstMasterNames:
+        print strName
+        
+#end Main()
 
-def DoWork():
-  
+main()
